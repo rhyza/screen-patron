f27cf76d35a53cc6a7552483ab8eda64aa9bec63
rhyza <rhyza.velasco@gmail.com>
2024-02-16 06:56:36 -0500

    Refactor code

--- app/components/InputImage.tsx
+++ app/components/InputImage.tsx
@@ -3,7 +3,7 @@ import React, { useRef, useState } from 'react';
 import { validateFile } from '~/utils';
 import EditIcon from './Icons';
 
-export default function InputImage() {
+export default function InputImage({ size = 200 }) {
   const [imageSrc, setImageSrc] = useState('');
   const handleImagePreview = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (validateFile(event, 5)) {
@@ -30,13 +30,13 @@ export default function InputImage() {
         type='file'
       />
       <Image
-        className='object-cover h-[200px]'
-        fallbackSrc='https://via.placeholder.com/200x200'
-        height={200}
+        className={`object-cover h-[${size}px]`}
+        fallbackSrc={`https://via.placeholder.com/${size}x${size}`}
+        height={size}
         id='image-preview'
         radius='none'
         src={imageSrc}
-        width={200}
+        width={size}
       />
       <CardFooter className="overflow-hidden absolute justify-end inset-x-0 bottom-0 text-small z-10">
         <div aria-hidden='true' className='rounded-full p-2 bg-gradient-to-r from-cyan-500 to-blue-500'>
