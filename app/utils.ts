97308b47542ed99843e9ab528813c065f92a0c66
rhyza <rhyza.velasco@gmail.com>
2024-02-15 23:27:28 -0500

    Add utility functions 'invariant' and 'validateFile'

---
+++ app/utils.ts
@@ -0,0 +1,48 @@
+const isProduction: boolean = process.env.NODE_ENV === 'production';
+
+/**
+ * Throws an error if the condition fails.
+ */
+export default function invariant(
+  condition: any,
+  // Can provide a string, or a function that returns a string for cases where
+  // the message takes a fair amount of effort to compute
+  message?: string | (() => string),
+): asserts condition {
+  if (condition) {
+    return;
+  }
+  // Condition not passed
+
+  const prefix: string = 'Invariant failed';
+
+  // In production we strip the message but still throw
+  if (isProduction) {
+    throw new Error(prefix);
+  }
+
+  // When not in production we allow the message to pass through
+  // *This block will be removed in production builds*
+
+  const provided: string | undefined = typeof message === 'function' ? message() : message;
+
+  // Options:
+  // 1. message provided: `${prefix}: ${provided}`
+  // 2. message not provided: prefix
+  const value: string = provided ? `${prefix}: ${provided}` : prefix;
+  throw new Error(value);
+}
+
+/**
+ * Checks if file exists and is less than a certain file size.
+ * @param event File Input event
+ * @param fileLimit File size limit in MB
+ */
+export function validateFile(event: React.ChangeEvent<HTMLInputElement>, fileLimit: number) {
+  const files = event.target.files || [];
+  if (files.length === 0 || files[0].size > fileLimit * 1000000) {
+    event.target.value = '';
+    return false;
+  }
+  return true;
+}
