8f696d8058c3d8ea814ad60a0a067079b6e15e92
rhyza <rhyza.velasco@gmail.com>
2024-02-19 02:56:47 -0500

    Add bio and social links to user profile

--- app/routes/user.$userId.tsx
+++ app/routes/user.$userId.tsx
@@ -1,10 +1,31 @@
-import { Button } from '@nextui-org/react';
+import { Button, Link } from '@nextui-org/react';
+import { InstagramIcon, LinkIcon, TwitterIcon } from '~/components/Icons';
 import InputImage from '~/components/InputImage';
 
 export default function User() {
   const isUser = true;
   const name = 'Anonymous Patron';
   const photo = 'https://placehold.co/800?text=Profile+Picture&font=roboto';
+  const bio = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+  const instagram = undefined; //'rhyza_';
+  const twitter = undefined; //'remix_run';
+  const website = 'https://tailwindcss.com/';
+
+  const socialChip = (label: string, url: string, icon: JSX.Element) => {
+    return (
+      <Button
+        as={Link}
+        className='bg-gradient-to-br from-indigo-500 to-pink-500 text-white'
+        href={url}
+        isExternal
+        radius='full'
+        size='sm'
+        startContent={icon}
+      >
+        {label}
+      </Button>
+    );
+  };
 
   return (
     <div className='w-full p-6'>
@@ -28,10 +49,24 @@ export default function User() {
         </div>
       </div>
       <div className='flex-auto max-w-96 text-center md:text-left'>
-        <div className='flex items-center md:h-[16rem] w-fit'>
+        <div className='flex items-center md:h-[16rem] w-fit mb-4 md:m-0'>
           <h1 className='text-6xl md:text-7xl font-medium'>{name}</h1>
         </div>
-        <p className='my-6 md:m-0'>Joined Feb '24</p>
+        {true && <p className='mb-4'>{bio}</p>}
+        {(instagram || twitter || website) &&
+          <div className='flex gap-2 mb-4 justify-center md:justify-start'>
+            {instagram &&
+              socialChip(`@${instagram}`, `https://www.instagram.com/${instagram}`, <InstagramIcon className='w-4 h-4'/>)
+            }
+            {twitter &&
+              socialChip(`@${twitter}`, `https://www.twitter.com/${twitter}`, <TwitterIcon className='w-4 h-4'/>)
+            }
+            {website &&
+              socialChip('Website', website, <LinkIcon className='w-5 h-5'/>)
+            }
+          </div>
+        }
+        <p className='mb-4'>Joined Feb '24</p>
       </div>
     </div>
   </div>
