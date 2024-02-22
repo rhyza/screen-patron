ed74a2c9b3dcaec3e5d3b25ad2d0942e95cbb25b
rhyza <rhyza.velasco@gmail.com>
2024-02-22 03:33:51 -0500

    Populate grid of screening cards

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+import ScreeningCard from '~/components/ScreeningCard';
+import type { ScreeningRecord } from '~/services/screening';
+import { getScreenings } from '~/services/screening';
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +13,29 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const screenings = await getScreenings(q);
+  return json({ screenings, q });
+};
+
 export default function Index() {
+  const { screenings, q } = useLoaderData<typeof loader>();
+
   return (
-    <div>
-      <h1 className='text-2xl'>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/blog'
-            rel='noreferrer'
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/jokes'
-            rel='noreferrer'
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className='w-full p-6 flex justify-center'>
+      {screenings.length ? (
+        <div className='max-w-[70rem] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+          {screenings.map((screening: ScreeningRecord) => (
+            <div key={screening.id} className='flex max-w-[400px] min-w-64'>
+              <ScreeningCard screening={screening} />
+            </div>
+          ))}
+        </div>
+      ) :
+        <p><i>No screenings</i></p>
+      }
     </div>
   );
 }
