7f7fc45d614b146cd67d2496c8dfc8829cebb5bd
rhyza <rhyza.velasco@gmail.com>
2024-03-02 18:01:22 -0500

    Scale hero text across resolutions and orientations

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -41,13 +41,23 @@ export default function Index() {
 
   return (
     <div className='container overflow-clip overscroll-none py-10'>
-        <div className='text-balance'>
+        <div
+          className={cn(
+            'absolute z-10 landscape:bottom-0 left-0 text-balance',
+            'portrait:top-1/4',
+            'lg:pr-12'
+          )}
+        >
           <h1
             className={cn(
-              'absolute z-10 landscape:bottom-0 left-0 portrait:pt-8',
-              'object-contain overflow-hidden bg-clip-text',
-              'text-6xl sm:text-6xl md:text-7xl lg:text-9xl 2xl:text-[10rem]',
-              'min-[2000px]:text-[12rem] min-[2600px]:text-[14rem] min-[2800px]:text-[16rem]',
+              'landscape:text-5xl sm:landscape:text-6xl md:landscape:text-7xl',
+              'text-6xl sm:portrait:text-8xl md:portrait:text-8xl',
+              'lg:landscape:text-8xl xl:landscape:text-9xl 2xl:landscape:text-[10rem]',
+              'lg:portrait:text-9xl xl:portrait:text-[10rem] 2xl:portrait:text-[12rem]',
+              'min-[2000px]:landscape:text-[12rem] min-[2600px]:landscape:text-[14rem]',
+              'min-[2000px]:portrait:text-[14rem] min-[2600px]:portrait:text-[16rem]',
+              'min-[2800px]:landscape:text-[16rem] min-[3400px]:landscape:text-[18rem]',
+              'min-[2800px]:portrait:text-[18rem] min-[3400px]:portrait:text-[20rem]',
               'font-extrabold uppercase',
               'selection:bg-fuchsia-300 selection:text-fuchsia-900'
             )}
@@ -61,13 +71,24 @@ export default function Index() {
             </NavLink>
           </h1>
         </div>
-        <img
+        <div
           className={cn(
-            'absolute z-0 right-0 mix-blend-exclusion saturate-[.85]',
-            'h-[20rem] sm:h-[30rem] md:h-[35rem] lg:h-[40rem] p-8',
+            'absolute z-0 landscape:bottom-1/4 right-0',
+            'portrait:top-0',
+            'min-[2000px]:left-1/2',
           )}
-          src={srcMap[counter % srcMap.length]}
-        />
+        >
+          <img
+            className={cn(
+              'h-[20rem] sm:h-[30rem] md:h-[35rem]',
+              'lg:h-[40rem] xl:h-[45rem] 2xl:h-[50rem]',
+              'min-[2000px]:h-[55rem] min-[2600px]:h-[60rem]',
+              'min-[2800px]:h-[65rem] min-[3400px]:h-[80rem]',
+              'object-contain p-8 saturate-[.85]',
+            )}
+            src={srcMap[counter % srcMap.length]}
+          />
+        </div>
     </div>
   );
 }
