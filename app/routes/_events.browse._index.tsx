c5269176eca36f8342cd9a3a4b432c8e5dbfc04b
rhyza <rhyza.velasco@gmail.com>
2024-03-01 19:29:30 -0500

    Make location filter button an input

--- app/routes/_events.browse._index.tsx
+++ app/routes/_events.browse._index.tsx
@@ -1,7 +1,7 @@
 import type { LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-import { Button } from '@nextui-org/react';
+import { Autocomplete, AutocompleteItem, Button } from '@nextui-org/react';
 
 import EventCards from '~/components/EventCards';
 import { getEvents } from '~/services/event';
@@ -15,11 +15,33 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 export default function BrowseEvents() {
   const { events, q } = useLoaderData<typeof loader>();
+  const cities = [
+    {id: 'nyc', name: 'New York, NY'},
+    {id: 'la', name: 'Los Angeles, CA'},
+    {id: 'chi', name: 'Chicago, IL'}
+  ];
 
   return (
     <>
       <div className='flex gap-2 p-2'>
-        <Button radius='full'>New York, NY</Button>
+        <Autocomplete
+          allowsCustomValue
+          classNames={{
+            base: 'max-w-44',
+          }}
+          defaultItems={cities}
+          defaultSelectedKey='nyc'
+          inputProps={{
+            classNames: {
+              input: 'ml-1',
+              inputWrapper: 'h-10 bg-default',
+            },
+          }}
+          isClearable={false}
+          radius='full'
+        >
+          {(item) => <AutocompleteItem key={item.id}>{item.name}</AutocompleteItem>}
+        </Autocomplete>
         <Button radius='full'>Date</Button>
         <Button radius='full'>Cost</Button>
       </div>
