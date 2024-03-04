94bae2caedea5bf98f029ac9c4601569fc2e2d84
rhyza <rhyza.velasco@gmail.com>
2024-03-03 21:27:31 -0500

    Format code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -65,22 +65,20 @@ export default function Index() {
             'min-[2000px]:left-1/2',
           )}
         >
-          {
-            heroImages.map((src, i) => (
-              <img
-                className={cn(
-                  'h-[20rem] sm:h-[30rem] md:h-[35rem]',
-                  'lg:h-[40rem] xl:h-[45rem] 2xl:h-[50rem]',
-                  'min-[2000px]:h-[55rem] min-[2600px]:h-[60rem]',
-                  'min-[2800px]:h-[65rem] min-[3400px]:h-[80rem]',
-                  'object-contain p-8 saturate-[.85]',
-                  (counter % heroImages.length === i) ? '' : 'hidden'
-                )}
-                key={i}
-                src={src}
-              />
-            ))
-          }
+          {heroImages.map((src, i) => (
+            <img
+              className={cn(
+                'h-[20rem] sm:h-[30rem] md:h-[35rem]',
+                'lg:h-[40rem] xl:h-[45rem] 2xl:h-[50rem]',
+                'min-[2000px]:h-[55rem] min-[2600px]:h-[60rem]',
+                'min-[2800px]:h-[65rem] min-[3400px]:h-[80rem]',
+                'object-contain p-8 saturate-[.85]',
+                (counter % heroImages.length === i) ? '' : 'hidden'
+              )}
+              key={i}
+              src={src}
+            />
+          ))}
         </div>
     </div>
   );
