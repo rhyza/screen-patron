e8b518277438cd31a68da1e51d1e130b86717822
rhyza <rhyza.velasco@gmail.com>
2024-03-10 17:29:50 -0400

    Add queries to promote a Guest to a Host and vice versa

--- app/models/host.server.ts
+++ app/models/host.server.ts
@@ -88,14 +88,74 @@ export async function updateHost(
 }
 
 /**
- * Remove a Host from an Event. If the Host is the only Host for an Event, then an error is
- * thrown unless otherwise specified.
- * @param eventId Event to remove Host from
- * @param userId User to remove as Host from Event
- * @param deleteSoloHostedEvent Defaults to false. If true, deletes the Event if the User is
- * the only Host for the Event, and returns the deleted Event.
- * @returns Host that was removed, unless Host was the sole host and deleteSoloHostedEvent is
- * true, then returns the Event deleted.
+ * Removes a User from an Event's guest list, and instead adds them as a Host.
+ * The associated RSVP record is deleted and the newly created Host record is returned.
+ * @requires `eventId`, `userId`
+ * @returns The newly created Host record
+ */
+export async function promoteToHost(eventId: Host['eventId'], userId: Host['userId']) {
+  const guest = await prisma.rsvp.delete({
+    where: {
+      id: { eventId, userId },
+    },
+    select: {
+      name: true,
+    },
+  });
+  const name = guest?.name;
+
+  return prisma.host.create({
+    data: {
+      eventId,
+      userId,
+      name,
+    },
+  });
+}
+
+/**
+ * Removes a User as an Event's host, and instead adds them to the Event's guest list along
+ * with an RSVP response.
+ * The associated Host record is deleted and the newly created RSVP record is returned.
+ * > If the User is the only Host for the Event then an error is thrown.
+ * @requires `eventId`, `userId`
+ * @param status (optional) The User's RSVP response, defaults to `Status.GOING`
+ * @returns The newly created RSVP record
+ */
+export async function demoteToGuest(
+  eventId: Host['eventId'],
+  userId: Host['userId'],
+  status = Status.GOING,
+) {
+  const host = await prisma.host.findUnique({
+    where: {
+      id: { eventId, userId },
+    },
+    select: {
+      name: true,
+    },
+  });
+  const name = host?.name;
+  removeHost(eventId, userId);
+
+  return prisma.rsvp.create({
+    data: {
+      eventId,
+      userId,
+      status,
+      name,
+    },
+  });
+}
+
+/**
+ * Removes a Host from an Event.
+ * > If the Host is the only Host for the Event and `deleteSoloHostedEvent` is `false`,
+ * then an error is thrown.
+ * @requires `eventId`, `userId`
+ * @param deleteSoloHostedEvent (optional) Specifies whether to delete the Event record if
+ * the User is the sole host, defaults to `false`
+ * @returns The Event record that was updated or deleted
  */
 export async function removeHost(
   eventId: Host['eventId'],
