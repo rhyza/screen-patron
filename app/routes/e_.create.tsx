4cb4d7a395f6179e8509bfbe9a4d5e60d6d9f252
rhyza <rhyza.velasco@gmail.com>
2024-03-21 01:53:17 -0400

    Attach new event to currently signed in user

--- app/routes/e_.create.tsx
+++ app/routes/e_.create.tsx
@@ -2,6 +2,7 @@ import type { ActionFunctionArgs, MetaFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 
 import EventForm from '~/components/EventForm';
+import { getSession, getSupabaseServerClient } from '~/db.server';
 import { createEvent } from '~/models/event.server';
 import { invariant } from '~/utils';
 
@@ -13,11 +14,12 @@ export const meta: MetaFunction = () => {
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const userId = 'test'; // replace
-  invariant(userId, "Missing signed in user's id");
+  const { supabase } = getSupabaseServerClient(request);
+  const session = await getSession(supabase);
+  invariant(session?.user?.id, "Missing signed in user's id");
   const formData = await request.formData();
   const values = Object.fromEntries(formData);
-  const event = await createEvent(userId, { ...values });
+  const event = await createEvent(session?.user?.id, { ...values });
   return redirect(`/e/${event.id}`);
 };
 
