c55bd26728c3258efedcc0aa9b24124b27c1091c
rhyza <rhyza.velasco@gmail.com>
2024-03-23 13:34:24 -0400

    SPO-85 Create empty "no screenings" state

--- app/components/EventCards.tsx
+++ app/components/EventCards.tsx
@@ -1,4 +1,5 @@
 import EventCard from './EventCard';
+import NewEventCard from './NewEventCard';
 import type { EventInfo } from '~/models/event.server';
 
 /**
@@ -8,19 +9,17 @@ import type { EventInfo } from '~/models/event.server';
 export default function EventCards({ events = [] }: { events: EventInfo[] }) {
   return (
     <div>
-      {events.length ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {events.map((event: EventInfo) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {events.length ? (
+          events.map((event: EventInfo) => (
             <div key={event.id} className="flex max-w-[400px] min-w-64">
               <EventCard event={event} />
             </div>
-          ))}
-        </div>
-      ) : (
-        <p>
-          <i>No Screenings</i>
-        </p>
-      )}
+          ))
+        ) : (
+          <NewEventCard />
+        )}
+      </div>
     </div>
   );
 }
