34a206252de5265e18a2a7b6fbc0a4c3263ef67d
rhyza <rhyza.velasco@gmail.com>
2024-03-26 18:51:51 -0400

    Refactor User Profile as a component

---
+++ app/components/UserProfile.tsx
@@ -0,0 +1,104 @@
+import { NavLink } from '@remix-run/react';
+import { Button, Link } from '@nextui-org/react';
+
+import { userPlaceholderImage } from '~/assets';
+import { InstagramIcon, LinkIcon, TwitterIcon } from '~/components/Icons';
+
+type UserProfileProps = {
+  isUser?: boolean;
+} & UserProfileValues;
+
+type UserProfileValues = {
+  name?: string | null;
+  photo?: string | null;
+  bio?: string | null;
+  instagram?: string | null;
+  twitter?: string | null;
+  website?: string | null;
+};
+
+/**
+ * Displays a User's name, bio, profile photo, instagram handle, twitter handle,
+and website link.
+ * @param UserProfileValues Any User info to show
+ * @param isUser Determines whether a user is viewing their own profile
+ */
+export default function UserProfile({
+  name,
+  photo,
+  bio,
+  instagram,
+  twitter,
+  website,
+  isUser,
+}: UserProfileProps) {
+  return (
+    <div className="flex flex-wrap md:flex-nowrap gap-6 justify-center">
+      <div className="flex-auto space-y-6 max-w-fit min-w-80">
+        <div className="flex justify-center">
+          <img
+            alt={name ? `${name}'s profile photo` : `Anonymous Patron's profile photo`}
+            className="rounded-full object-cover size-64"
+            src={photo || userPlaceholderImage}
+          />
+        </div>
+        <div className="flex justify-center">
+          {isUser && (
+            <Button as={NavLink} className="w-64" to="./edit" radius="none">
+              Edit
+            </Button>
+          )}
+        </div>
+      </div>
+      <div className="flex-auto max-w-96 text-center md:text-left">
+        <div className="flex items-center md:h-64 w-fit mb-4 md:m-0">
+          <h1 className="text-6xl md:text-7xl font-medium">{name || 'Anonymous Patron'}</h1>
+        </div>
+        <div className="grid gap-4">
+          {bio && <p>{bio}</p>}
+          {(instagram || twitter || website) && (
+            <div className="flex gap-2 justify-center md:justify-start">
+              {instagram && (
+                <SocialChip
+                  label={`@${instagram}`}
+                  url={`https://www.instagram.com/${instagram}`}
+                  icon={<InstagramIcon className="w-4 h-4" />}
+                />
+              )}
+              {twitter && (
+                <SocialChip
+                  label={`@${twitter}`}
+                  url={`https://www.twitter.com/${twitter}`}
+                  icon={<TwitterIcon className="w-4 h-4" />}
+                />
+              )}
+              {website && (
+                <SocialChip
+                  label="Website"
+                  url={`https://${website}`}
+                  icon={<LinkIcon className="w-5 h-5" />}
+                />
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SocialChip({ label, url, icon }: { label: string; url: string; icon: JSX.Element }) {
+  return (
+    <Button
+      as={Link}
+      className="bg-gradient"
+      href={url}
+      isExternal
+      radius="full"
+      size="sm"
+      startContent={icon}
+    >
+      {label}
+    </Button>
+  );
+}
--- app/routes/user.$userId.tsx
+++ app/routes/user.$userId.tsx
@@ -1,10 +1,8 @@
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { NavLink, useLoaderData, useOutletContext } from '@remix-run/react';
-import { Button, Link } from '@nextui-org/react';
+import { useLoaderData, useOutletContext } from '@remix-run/react';
 
-import { userPlaceholderImage } from '~/assets';
-import { InstagramIcon, LinkIcon, TwitterIcon } from '~/components/Icons';
+import UserProfile from '~/components/UserProfile';
 import type { OutletContext } from '~/db.server';
 import type { User } from '~/models/user.server';
 import { getUser } from '~/models/user.server';
@@ -32,77 +30,18 @@ export default function UserPage() {
     user: { id, name, photo, bio, instagram, twitter, website },
   } = useLoaderData<typeof loader>();
   const session = useOutletContext<OutletContext>();
-  const isUser = session?.user?.id === id;
 
   return (
     <div className="w-full p-6">
-      <div className="flex flex-wrap md:flex-nowrap gap-6 justify-center">
-        <div className="flex-auto space-y-6 max-w-fit min-w-80">
-          <div className="flex justify-center">
-            <img
-              alt={name ? `${name}'s profile photo` : `Anonymous Patron's profile photo`}
-              className="rounded-full object-cover size-64"
-              src={photo || userPlaceholderImage}
-            />
-          </div>
-          <div className="flex justify-center">
-            {isUser && (
-              <Button as={NavLink} className="w-64" to="./edit" radius="none">
-                Edit
-              </Button>
-            )}
-          </div>
-        </div>
-        <div className="flex-auto max-w-96 text-center md:text-left">
-          <div className="flex items-center md:h-64 w-fit mb-4 md:m-0">
-            <h1 className="text-6xl md:text-7xl font-medium">{name || 'Anonymous Patron'}</h1>
-          </div>
-          <div className="grid gap-4">
-            {bio && <p>{bio}</p>}
-            {(instagram || twitter || website) && (
-              <div className="flex gap-2 justify-center md:justify-start">
-                {instagram && (
-                  <SocialChip
-                    label={`@${instagram}`}
-                    url={`https://www.instagram.com/${instagram}`}
-                    icon={<InstagramIcon className="w-4 h-4" />}
-                  />
-                )}
-                {twitter && (
-                  <SocialChip
-                    label={`@${twitter}`}
-                    url={`https://www.twitter.com/${twitter}`}
-                    icon={<TwitterIcon className="w-4 h-4" />}
-                  />
-                )}
-                {website && (
-                  <SocialChip
-                    label="Website"
-                    url={`https://${website}`}
-                    icon={<LinkIcon className="w-5 h-5" />}
-                  />
-                )}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
+      <UserProfile
+        name={name}
+        photo={photo}
+        bio={bio}
+        instagram={instagram}
+        twitter={twitter}
+        website={website}
+        isUser={session?.user?.id === id}
+      />
     </div>
-  );
-}
-
-function SocialChip({ label, url, icon }: { label: string; url: string; icon: JSX.Element }) {
-  return (
-    <Button
-      as={Link}
-      className="bg-gradient"
-      href={url}
-      isExternal
-      radius="full"
-      size="sm"
-      startContent={icon}
-    >
-      {label}
-    </Button>
   );
 }
