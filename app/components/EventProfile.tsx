3e78459448f59e3d0b470b306be581c979e63f19
rhyza <rhyza.velasco@gmail.com>
2024-03-27 18:46:19 -0400

    Fix bug showing people who aren't going as guests

--- app/components/EventProfile.tsx
+++ app/components/EventProfile.tsx
@@ -139,16 +139,18 @@ function Guests({ guests, guestCount }: { guests: RsvpInfo[]; guestCount: GuestC
         {guestCount.MAYBE > 0 && <span className="flex-none">{guestCount.MAYBE} Maybe</span>}
       </div>
       <div className="flex items-center gap-2">
-        {guests.map((guest, index) => {
-          return (
-            <Tooltip
-              content={retypeNull(guest.name, guest.user.name) || 'Attendee'}
-              key={index}
-            >
-              <Avatar showFallback src={retypeNull(guest.user.photo)} />
-            </Tooltip>
-          );
-        })}
+        {guests
+          .filter((guest) => guest.status != 'NOT_GOING')
+          .map((guest, index) => {
+            return (
+              <Tooltip
+                content={retypeNull(guest.name, guest.user.name) || 'Attendee'}
+                key={index}
+              >
+                <Avatar showFallback src={retypeNull(guest.user.photo)} />
+              </Tooltip>
+            );
+          })}
         {guestCount.TOTAL_GUESTS > 6 && <Avatar name={`+${guestCount.TOTAL_GUESTS - 6}`} />}
       </div>
     </>
