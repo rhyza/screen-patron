758ca2b10f2ad7adcc4356eaf6f67d14326a9786
rhyza <rhyza.velasco@gmail.com>
2024-03-27 20:45:13 -0400

    Use refactored EventInfoForm and EventSettings

--- app/components/EventForm.tsx
+++ app/components/EventForm.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { Form, useNavigate } from '@remix-run/react';
-import { Button, Input, Textarea } from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 
-import { MapPinIcon, TicketIcon, UserGroupIcon } from './Icons';
+import { ButtonTab, ButtonTabs } from './ButtonTabs';
+import EventInfoForm from './EventInfoForm';
+import EventSettings from './EventSettings';
 import InputImage from './InputImage';
 import { eventPlaceholderImage } from '~/assets';
-import { getDateInputString } from '~/utils';
 
 type EventFormProps = {
+  defaultTab?: string;
   isDisabled?: boolean;
 } & EventFormValues;
 
@@ -29,54 +31,18 @@ type EventFormValues = {
  * @param isDisabled Sets submit button to disabled
  */
 export default function EventForm({
-  name,
+  defaultTab = 'info',
   photo,
-  dateStart,
-  dateEnd,
-  location,
-  cost,
-  capacity,
-  description,
   isDisabled = false,
+  ...eventFormValues
 }: EventFormProps) {
   const navigate = useNavigate();
-  const today = getDateInputString(new Date(Date.now()));
-  const start = dateStart ? getDateInputString(dateStart) : '';
-  const end = dateEnd ? getDateInputString(dateEnd) : '';
-
-  const [errorMessage, setErrorMessage] = useState('');
+  const [tab, setTab] = useState(defaultTab);
   const [submitDisabled, setSubmitDisabled] = useState(isDisabled);
-  const [showEndDateInput, setShowEndDateInput] = useState(end != '');
 
-  const [dateStartValue, setDateStartValue] = useState(start);
-  const handleDateStartInput = (value: string) => {
-    setDateStartValue(() => value);
-    validateDateInputs(value, dateEndValue);
-  };
-
-  const [dateEndValue, setDateEndValue] = useState(end);
-  const handleDateEndInput = (value: string) => {
-    setDateEndValue(() => value);
-    validateDateInputs(dateStartValue, value);
-  };
-
-  const validateDateInputs = (startValue: string, endValue: string) => {
-    if (!endValue) {
-      setSubmitDisabled(() => false);
-      setErrorMessage(() => '');
-    } else if (!startValue) {
-      setSubmitDisabled(() => true);
-      setErrorMessage(() => `You can't have an end date without a start date.`);
-    } else if (new Date(startValue) < new Date(Date.now())) {
-      setSubmitDisabled(() => true);
-      setErrorMessage(() => `You can't a start date in the past.`);
-    } else if (new Date(startValue) < new Date(endValue)) {
-      setSubmitDisabled(() => false);
-      setErrorMessage(() => '');
-    } else {
-      setSubmitDisabled(() => true);
-      setErrorMessage(() => 'The end date is before the start date.');
-    }
+  const setTabContent = (id: string) => {
+    setTab(() => id);
+    return id;
   };
 
   return (
@@ -86,94 +52,14 @@ export default function EventForm({
       method="post"
     >
       <div className="flex-auto space-y-2 min-w-[300px] max-w-xl">
-        <Input
-          classNames={{
-            label: [
-              'text-3xl group-data-[filled-within=true]:scale-[.475]',
-              'group-data-[filled-within=true]:-translate-y-[calc(38%_+_theme(fontSize.small)/2_-_8px)]',
-            ],
-            inputWrapper: 'h-20 bg-subtle data-[hover=true]:bg-subtle-hover',
-            input: 'text-3xl',
-          }}
-          defaultValue={name}
-          label="Event Name"
-          name="name"
-          radius="none"
-          size="lg"
-          type="text"
-        />
-        <Input
-          classNames={{ inputWrapper: 'bg-subtle data-[hover=true]:bg-subtle-hover' }}
-          label="Start Date"
-          min={today}
-          name="dateStart"
-          onValueChange={handleDateStartInput}
-          placeholder="TBD"
-          radius="none"
-          type="datetime-local"
-          value={dateStartValue}
-        />
-        {showEndDateInput && (
-          <Input
-            classNames={{ inputWrapper: 'bg-subtle data-[hover=true]:bg-subtle-hover' }}
-            errorMessage={errorMessage}
-            label="End Date"
-            min={today}
-            name="dateStart"
-            onValueChange={handleDateEndInput}
-            placeholder="TBD"
-            radius="none"
-            type="datetime-local"
-            value={dateEndValue}
-          />
+        <ButtonTabs className="pb-2" defaultTab="info" setTabContent={setTabContent}>
+          <ButtonTab id="info">Event Info</ButtonTab>
+          <ButtonTab id="settings">Settings</ButtonTab>
+        </ButtonTabs>
+        {tab === 'info' && (
+          <EventInfoForm setSubmitDisabled={setSubmitDisabled} {...eventFormValues} />
         )}
-        {dateStartValue && !showEndDateInput && (
-          <Button
-            className="btn-link"
-            onPress={() => setShowEndDateInput(() => !showEndDateInput)}
-          >
-            Add end time
-          </Button>
-        )}
-        <Input
-          classNames={{ inputWrapper: 'bg-subtle data-[hover=true]:bg-subtle-hover' }}
-          defaultValue={location}
-          placeholder="Location"
-          name="location"
-          radius="none"
-          startContent={<MapPinIcon />}
-          type="text"
-        />
-        <Input
-          classNames={{ inputWrapper: 'bg-subtle data-[hover=true]:bg-subtle-hover' }}
-          defaultValue={`${capacity}`}
-          name="capacity"
-          placeholder="Max Capacity"
-          radius="none"
-          startContent={<UserGroupIcon />}
-          type="number"
-        />
-        <Input
-          classNames={{ inputWrapper: 'bg-subtle data-[hover=true]:bg-subtle-hover' }}
-          defaultValue={`${cost}`}
-          name="cost"
-          placeholder="Cost per person"
-          radius="none"
-          startContent={
-            <>
-              <TicketIcon />
-              <span className="ml-1">$</span>
-            </>
-          }
-          type="number"
-        />
-        <Textarea
-          classNames={{ inputWrapper: 'bg-subtle data-[hover=true]:bg-subtle-hover' }}
-          defaultValue={description}
-          name="description"
-          label="Description"
-          radius="none"
-        />
+        {tab === 'settings' && <EventSettings />}
       </div>
       <div className="flex-auto justify-center space-y-6 max-w-80 sm:max-w-96">
         <InputImage
--- app/components/EventSettings.tsx
+++ app/components/EventSettings.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@nextui-org/react';
+
 export default function EventSettings() {
-  return <div></div>;
+  return (
+    <div className='px-1 py-8'>
+      <Button color='danger' radius='none'>Cancel Event</Button>
+    </div>
+  );
 }
