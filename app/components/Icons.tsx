d75c1c48d92e2ceb9b182aca42e1f76bd514869f
rhyza <rhyza.velasco@gmail.com>
2024-04-08 19:49:28 -0400

    Add new Prohibited Symbol Icon

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -201,6 +201,18 @@ export function PlayIcon(props: IconProps) {
   );
 }
 
+export function ProhibitedIcon(props: IconProps) {
+  return (
+    <Svg {...props}>
+      <path
+        fillRule="evenodd"
+        d="m6.72 5.66 11.62 11.62A8.25 8.25 0 0 0 6.72 5.66Zm10.56 12.68L5.66 6.72a8.25 8.25 0 0 0 11.62 11.62ZM5.105 5.106c3.807-3.808 9.98-3.808 13.788 0 3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788Z"
+        clipRule="evenodd"
+      />
+    </Svg>
+  );
+}
+
 export function StarIcon(props: IconProps) {
   return (
     <Svg {...props}>
