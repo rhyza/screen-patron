f2ef9533fe99efaf0347f2c464ef96846a742599
rhyza <rhyza.velasco@gmail.com>
2024-04-07 20:24:18 -0400

    Make CostPicker a controlled component

--- app/components/CostPicker.tsx
+++ app/components/CostPicker.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { Button, Popover, PopoverContent, PopoverTrigger, Slider } from '@nextui-org/react';
 
 import { DollarIcon } from './Icons';
 
 export default function CostPicker() {
+  const [range, setRange] = useState<number[]>([0, 100]);
+  const getPriceRange = () => {
+    const [min, max] = range;
+    if (!min && !max) {
+      return 'Free Only'
+    } else if (!min && max === 100) {
+      return 'No Limits'
+    } else {
+      const minString = min ? `$${min}` : 'Free';
+      const maxString = max < 100 ? `$${max}` : 'Any';
+      return minString + ' — ' + maxString;
+    }
+  }
+
   return (
-    <Popover placement="bottom" showArrow offset={10}>
+    <Popover backdrop="opaque" offset={10} placement="bottom">
       <PopoverTrigger>
         <Button
           className="max-md:px-unit-0 max-md:min-w-unit-10"
@@ -14,16 +29,19 @@ export default function CostPicker() {
           <span className="max-md:hidden">Cost</span>
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[240px]">
+      <PopoverContent className="w-60 py-4 px-6 bg-foreground text-background">
         <Slider
-          label="Price Range"
+          arai-label="Price Range"
           step={1}
           minValue={0}
-          maxValue={200}
-          defaultValue={[0, 200]}
-          formatOptions={{ style: 'currency', currency: 'USD' }}
+          maxValue={100}
+          value={range}
+          onChange={(input) => typeof input != 'number' && setRange(input)}
           className="max-w-md"
         />
+        <span className="mt-4 text-small font-medium">
+          {getPriceRange()}
+        </span>
       </PopoverContent>
     </Popover>
   );
