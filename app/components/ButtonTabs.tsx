129a225441cb89cd82d85d710537b1e725f41742
rhyza <rhyza.velasco@gmail.com>
2024-04-10 19:43:39 -0400

    Allow ButtonTabs to be controlled or uncontrolled

--- app/components/ButtonTabs.tsx
+++ app/components/ButtonTabs.tsx
@@ -1,3 +1,4 @@
+import type { SetStateAction } from 'react';
 import { useState, createContext, useContext } from 'react';
 import { Button, cn } from '@nextui-org/react';
 
@@ -12,35 +13,48 @@ const ButtonTabContext = createContext({
  * Tab switcher composed of a row buttons. The buttons change the state controlled by the
  * given callback function. Each button will display an active state when pressed and will
  * maintain that state until a different button is pressed.
- * @param className Any Tailwind classes to apply to the container
- * @param defaultTab The `id` of the tab to be active on first render
- * @param setTabContent Function that sets what happens when each button is pressed
+ * @param className (optional) Any Tailwind classes to apply to the container
+ * @param defaultTab (optional) The `id` of the tab to be active on first render (uncontrolled)
+ * @param isDeselectable (optional) Allows buttons to be pressed again to deselect
+ * @param onTabChange (optional) Handler that is called when the tab changes
+ * @param setTabContent Handler that is called when the tab changes
+ * @param tab (optional) The `id` of the current tab (controlled)
  */
 export function ButtonTabs({
   children,
   className = '',
   defaultTab = '',
   isDeselectable = false,
+  onTabChange,
   setTabContent,
+  tab,
 }: {
   defaultTab?: string;
   isDeselectable?: boolean;
+  onTabChange?: (id: SetStateAction<string>) => void;
   setTabContent: (id: string) => void;
+  tab?: string;
 } & React.ComponentProps<'div'>) {
   const [activeTab, setActiveTab] = useState(defaultTab);
+  const isControlled = tab != undefined;
+
   const handlePress = (id: string) => {
-    if (isDeselectable && activeTab === id) {
-      setActiveTab(() => '');
+    if (isDeselectable && (isControlled ? tab === id : activeTab === id)) {
+      if (!isControlled) setActiveTab(() => '');
+      else if (onTabChange) onTabChange(() => '');
       setTabContent('');
     } else {
-      setActiveTab(() => id);
+      if (!isControlled) setActiveTab(() => id);
+      else if (onTabChange) onTabChange(() => id);
       setTabContent(id);
     }
   };
 
   return (
     <div className={cn('flex gap-2', className)}>
-      <ButtonTabContext.Provider value={{ activeTab, handlePress }}>
+      <ButtonTabContext.Provider
+        value={{ activeTab: isControlled ? tab : activeTab, handlePress }}
+      >
         {children}
       </ButtonTabContext.Provider>
     </div>
